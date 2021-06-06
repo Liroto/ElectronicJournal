97ed7aa343b892f410a95c76ba14760ca0dc2861
Liroto-laptop <Liroto@DESKTOP-1CGU99E>
2021-06-06 14:18:20 +0300

    Запускаю VS на ноутбуке

--- ElectronicJournal/Form1.Designer.cs
+++ ElectronicJournal/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace ElectronicJournal
 {
-    partial class Form1
+    partial class AuthorizationForm
     {
         /// <summary>
         /// Обязательная переменная конструктора.
@@ -28,13 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(287, 205);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(174, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "PLACEHOLDER-TEST";
+            // 
+            // AuthorizationForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.label1);
+            this.Name = "AuthorizationForm";
+            this.Text = "Authorization";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
     }
 }
 
