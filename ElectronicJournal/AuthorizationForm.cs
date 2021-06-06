b4cd83434fc487750ede13ffa6657896065fe9bd
Liroto-laptop <Liroto@DESKTOP-1CGU99E>
2021-06-06 16:15:48 +0300

    Создал main,authorization, и registration

--- ElectronicJournal/AuthorizationForm.cs
+++ ElectronicJournal/AuthorizationForm.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,18 @@ namespace ElectronicJournal
         public AuthorizationForm()
         {
             InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            RegistrationForm reg = new RegistrationForm();
+            reg.Show();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MainForm main = new MainForm();
+            main.Show();
         }
     }
 }
---
+++ ElectronicJournal/RegistrationForm.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ElectronicJournal
+{
+    public partial class RegistrationForm : Form
+    {
+        public RegistrationForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string userData = textBox1.Text + "," + textBox2.Text;
+            string path = @""+textBox1.Text + ".csv";
+            if (File.Exists(path) == true)
+            {
+                MessageBox.Show("Такой пользователь уже существует.");
+            }
+            else
+            {
+                File.WriteAllText(path, userData);
+                MessageBox.Show(textBox1.Text+", Вы успешно зарегистрированы!");
+                Close();
+            }
+        }
+    }
+}
